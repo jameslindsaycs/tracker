f9c3878c7317bfcfc94cfd20227781180d0940d6
jameslindsaycs <j.lindsay1@nuigalway.ie>
2019-11-24 23:13:09 +0000

    added validators to athlete model

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,2 +1,7 @@
 class Athlete < ApplicationRecord
+
+    validates :name, :description, :image_url, :strength, :speed, :agility, presence: true
+    validates :name, uniqueness: true
+    validates :strength, :speed, :agility, numericality: { greater_than_or_equal_to: 0.01 }
+
 end
